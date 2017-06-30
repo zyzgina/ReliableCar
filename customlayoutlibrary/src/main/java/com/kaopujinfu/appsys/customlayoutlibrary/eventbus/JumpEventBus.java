69721a529922b1f652c9a6d5de14b658dc8c49f2
左丽姬 <zyzgina@163.com>
2017-06-30 11:12:52 +0800

    靠谱看车

--- customlayoutlibrary/src/main/java/com/kaopujinfu/appsys/customlayoutlibrary/eventbus/JumpEventBus.java
+++ customlayoutlibrary/src/main/java/com/kaopujinfu/appsys/customlayoutlibrary/eventbus/JumpEventBus.java
@@ -6,11 +6,8 @@ package com.kaopujinfu.appsys.customlayoutlibrary.eventbus;
  */
 
 public class JumpEventBus {
-    private int status=0;//0、获取个人信息；1、上传完成；2、首页-》产品推荐 ; 3、侧滑菜单
-    //status=0x100 网络重新加载发送的广播 status=0x103 首页接受广播后继续发送
-    //status=0x101 首页最新资讯阅读次数发生改变 0x105 行业接受广播后继续发送
-    //status=0x102 行业资讯阅读次数发生改变  0x104 首页接受广播后继续发送
-    //status=0x106 行业资讯条件查询
+    private int status = 0;//0、获取个人信息；1、上传完成；2、首页-》产品推荐 ; 3、侧滑菜单
+    //status=0x100 网络重新加载发送的广播 status=0x103 盘库成功数据发生改变，文档绑定成功数据发生改变，监管器绑定成功数据发生改变
     private String name;//name="com.reliablel.retail.service.STOPSERVICE";  关闭服务
 
     public JumpEventBus() {
