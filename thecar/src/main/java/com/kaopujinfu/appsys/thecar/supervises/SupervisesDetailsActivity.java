49b81460c7a08db1fb459966e4126d81549a0493
左丽姬 <zyzgina@163.com>
2017-07-04 11:59:29 +0800

    靠谱看车

--- thecar/src/main/java/com/kaopujinfu/appsys/thecar/supervises/SupervisesDetailsActivity.java
+++ thecar/src/main/java/com/kaopujinfu/appsys/thecar/supervises/SupervisesDetailsActivity.java
@@ -32,7 +32,7 @@ public class SupervisesDetailsActivity extends BaseNoScoActivity {
     private SuperviseDetailsAdapter mAdapter;
     private SupervisersBean.SupervisersItemsEntity itemsEntity;
     private LinearLayout mNoDate;
-    private int maxTotal = 15;
+    private int maxTotal = 10;
     TextView alldetailsList;
 
     @Override
