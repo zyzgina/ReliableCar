d8b83dbe87f12a4b73508f36277c032822d7d1f6
左丽姬 <zyzgina@163.com>
2017-07-11 11:44:17 +0800

    靠谱看车-

--- thecar/src/main/java/com/kaopujinfu/appsys/thecar/bean/SupervisersBean.java
+++ thecar/src/main/java/com/kaopujinfu/appsys/thecar/bean/SupervisersBean.java
@@ -59,19 +59,24 @@ public class SupervisersBean implements Serializable {
     }
 
     public static class SupervisersItemsEntity implements Serializable {
+
         /**
          * shortName : 上海申协日产
          * longName : 上海申协日产有限公司
-         * wareHouseCount : 0
-         * companyCode : G-177004-0153
-         * arrcount : [{"status":"正常监管","count":"6"},{"status":"监管结束","count":"1"}]
+         * wareHouseCount : 3
+         * companyCode : G-177004-5472
+         * sum : 13
+         * status : 正常
+         * count : 13
          */
 
         private String shortName;
         private String longName;
         private String wareHouseCount;
         private String companyCode;
-        private List<ArrcountEntity> arrcount;
+        private int sum;
+        private String status;
+        private int count;
 
         public void setShortName(String shortName) {
             this.shortName = shortName;
@@ -89,8 +94,16 @@ public class SupervisersBean implements Serializable {
             this.companyCode = companyCode;
         }
 
-        public void setArrcount(List<ArrcountEntity> arrcount) {
-            this.arrcount = arrcount;
+        public void setSum(int sum) {
+            this.sum = sum;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
         }
 
         public String getShortName() {
@@ -109,53 +122,16 @@ public class SupervisersBean implements Serializable {
             return companyCode;
         }
 
-        public List<ArrcountEntity> getArrcount() {
-            return arrcount;
+        public int getSum() {
+            return sum;
         }
 
-        public static class ArrcountEntity implements Serializable {
-            /**
-             * status : 正常监管
-             * count : 6
-             */
-
-            private String status;
-            private String count;
-
-            public void setStatus(String status) {
-                this.status = status;
-            }
-
-            public void setCount(String count) {
-                this.count = count;
-            }
-
-            public String getStatus() {
-                return status;
-            }
-
-            public String getCount() {
-                return count;
-            }
-
-            @Override
-            public String toString() {
-                return "ArrcountEntity{" +
-                        "status='" + status + '\'' +
-                        ", count='" + count + '\'' +
-                        '}';
-            }
+        public String getStatus() {
+            return status;
         }
 
-        @Override
-        public String toString() {
-            return "SupervisersItemsEntity{" +
-                    "shortName='" + shortName + '\'' +
-                    ", longName='" + longName + '\'' +
-                    ", wareHouseCount='" + wareHouseCount + '\'' +
-                    ", companyCode='" + companyCode + '\'' +
-                    ", arrcount=" + arrcount +
-                    '}';
+        public int getCount() {
+            return count;
         }
     }
 
