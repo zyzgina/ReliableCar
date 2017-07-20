0646463f2abd2237407ea6d8c687bc3893611268
左丽姬 <zyzgina@163.com>
2017-07-20 17:46:33 +0800

    靠谱看车-添加声网初始化线程

--- customlayoutlibrary/src/main/java/com/kaopujinfu/appsys/customlayoutlibrary/RetailAplication.java
+++ customlayoutlibrary/src/main/java/com/kaopujinfu/appsys/customlayoutlibrary/RetailAplication.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import com.kaopujinfu.appsys.customlayoutlibrary.carsh.AppCrashHandler;
 import com.kaopujinfu.appsys.customlayoutlibrary.utils.GeneralUtils;
 import com.kaopujinfu.appsys.customlayoutlibrary.utils.SPUtils;
+import com.kaopujinfu.appsys.customlayoutlibrary.videocall.modle.WorkerThread;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -77,5 +78,29 @@ public class RetailAplication extends MultiDexApplication {
             }
             activities.clear();
         }
+    }
+
+    /* 声网 */
+    private WorkerThread mWorkerThread;
+    public synchronized void initWorkerThread() {
+        if (mWorkerThread == null) {
+            mWorkerThread = new WorkerThread(getApplicationContext());
+            mWorkerThread.start();
+            mWorkerThread.waitForReady();
+        }
+    }
+
+    public synchronized WorkerThread getWorkerThread() {
+        return mWorkerThread;
+    }
+
+    public synchronized void deInitWorkerThread() {
+        mWorkerThread.exit();
+        try {
+            mWorkerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mWorkerThread = null;
     }
 }
