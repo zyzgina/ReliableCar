a86d099c68983fe54a011b2070692b9a6ea62ac3
左丽姬 <zyzgina@163.com>
2017-08-14 14:42:24 +0800

    靠谱看车-添加车辆

--- thecar/src/main/java/com/kaopujinfu/appsys/thecar/menu/VerionActivity.java
+++ thecar/src/main/java/com/kaopujinfu/appsys/thecar/menu/VerionActivity.java
@@ -1,13 +1,19 @@
 package com.kaopujinfu.appsys.thecar.menu;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.kaopujinfu.appsys.customlayoutlibrary.activitys.BaseNoScoActivity;
 import com.kaopujinfu.appsys.customlayoutlibrary.tools.IBaseMethod;
 import com.kaopujinfu.appsys.thecar.R;
+import com.kaopujinfu.appsys.thecar.bean.VersionBean;
 
 /**
  * Describe: 版本更新
@@ -32,6 +38,33 @@ public class VerionActivity extends BaseNoScoActivity {
         header.setBackgroundColor(getResources().getColor(R.color.car_theme));
         TextView versionName= (TextView) findViewById(R.id.versionName);
         versionName.setText("当前版本号："+ IBaseMethod.getVersion(this));
+        VersionBean.VersionEntity versionEntity= (VersionBean.VersionEntity) getIntent().getSerializableExtra("versionEntity");
         TextView versionMsg= (TextView) findViewById(R.id.versionMsg);
+        Button btnVersion= (Button) findViewById(R.id.btnVersion);
+        if(versionEntity!=null){
+            PackageManager manager = this.getPackageManager();
+            PackageInfo info = null;
+            try {
+                info = manager.getPackageInfo(this.getPackageName(), 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            String version = info.versionName;
+            int vernum = version.compareTo(versionEntity.getAppVersion());
+            if (vernum < 0) {
+                btnVersion.setVisibility(View.VISIBLE);
+                versionMsg.setText("最新版本：v"+versionEntity.getAppVersion()+"\n\n更新内容：\n"+versionEntity.getChangeLog());
+            }else{
+                versionMsg.setText("当前版本：\n\n"+versionEntity.getChangeLog());
+            }
+        }
+        btnVersion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.25pp.com/android/detail_7621405/");
+                Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(it);
+            }
+        });
     }
 }
